51fff8e4313e22a78714cdc1e54e6aaf65b8e068
Flavio <flavio.finger@epfl.ch>
2016-10-26 13:05:30 +0200

    added plot of output

--- 26Oct/combineCSVs.py
+++ 26Oct/combineCSVs.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 flist=[]
 for filename in os.listdir('.'):
-    if filename.endswith(".csv"): 
+    if (filename.endswith(".csv") & (filename!="allDept.csv")): 
         flist=flist+[filename]
 
+dfl=[]
 for fn in flist:
     dfl.append(pd.read_csv(fn,sep='\t',encoding='utf-16',parse_dates=True,index_col=[0,1],usecols=[0,1,3]))
     dfl[-1]=dfl[-1].rename(columns = {'Nouveaux Cas':fn[:-4]})
@@ -17,3 +18,6 @@ df=df.reindex_axis(sorted(df.columns), axis=1)
 
 df.to_pickle('dataframeCombined.pickle')
 df.to_csv('allDept.csv')
+
+ax=df.plot(x=df.index.levels[0])
+ax.get_figure().savefig('output.png')
